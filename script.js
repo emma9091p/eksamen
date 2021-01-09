a209751191cf746e0fb04009929d1883a67cbb8f
Emma Noiesen <emmanoiesen@Emmas-MacBook-Pro.local>
2021-01-09 16:55:42 +0100

    rettelser menu virker

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 window.addEventListener("load", sidenVises);
 
+
+
 function sidenVises() {
     console.log("sidenVises");
-    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+    document.querySelector("#burgerknap").addEventListener("click", toggleMenu);
 }
 
 
+
 function toggleMenu() {
-    console.log("toggleMenu");
-    document.querySelector("#menu").classList.toggle("hidden");
 
     let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+    document.querySelector("#burgerfane").classList.toggle("hidden");
 
-    if (erSkjult == true) {
-        document.querySelector("#menuknap").textContent = "☰";
-    } else {
-        document.querySelector("#menuknap").textContent = "X";
-    }
+
 }
